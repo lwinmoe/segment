4735b7c9ad1b849818c14b9add24604af033420d
Lwin Moe <lwinmoe@gmail.com>
2016-04-09 11:43:17 -0400

    baseline.

--- experiment.py
+++ experiment.py
@@ -27,38 +27,45 @@ with open(inputF, 'r') as f:
         #if cnt > MAX:
         #    continue
 
-        print(line)
-        #output = os.system("./segment" + " burmese.fst '" + line + "'")
-        #output = call("./segment" + " burmese.fst '" + line + "'", shell=True)
+        print("Sentence:", line)
+
+        ####################### using segmenter ###################################
+        '''
         output = check_output(["./segment" + " burmese.fst '" + line + "'"], shell=True)
         outStr = output.decode('utf-8')
         print(outStr)
         outStr = outStr.replace(" ", "|")
         outStr = outStr.replace("||", "|")
-
         outStr = outStr.replace("\n", '')
-
         outItems = outStr.split("|")
+        '''
+        ####################### end using segmenter
+
+        ####################### baseline #########################
+        line = line.replace("\n", "")
+        outItems = line.split(" ")
+        ####################### end baseline #####################
+
         goldStr = gold_lines[cnt]
         goldStr = goldStr.replace("\n", '')
         goldItems = goldStr.split("|")
         goldItems = [x for x in goldItems if x]
         print("GOLD:", goldItems)
         outItems = [x for x in outItems if x]
-
         correctItems = [x for x in outItems if x in goldItems]
         if outItems:
             totalGold += len(goldItems)
             totalCorrect += len(correctItems)
             totalOut += len(outItems)
 
-            print(len(outItems))
-            print(len(correctItems))
+            print("Gold cnt:", len(goldItems))
+            print("Segmentation:", outItems)
+            print("Segmented items cnt:", len(outItems))
+            print("Correct items cnt:", len(correctItems))
             recall = len(outItems) / len(goldItems) * 100
             print("Recall:", recall)
             precision = len(correctItems) / len(goldItems) * 100
             print("Precision:", precision)
-            print(outItems)
             print("===========================\n")
         cnt += 1
 
