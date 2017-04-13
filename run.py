dd796dde5d8a92b02985ba036132c37d4e9a92bd
Lwin Moe <lwinmoe@gmail.com>
2017-04-13 09:20:28 -0400

    experiment and run scripts.

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+# INPUT: a file with Burmese sentences in each line.
+# OUTPUT: segmentation results to the terminal output (with two double spaces between words)
 import os
 from subprocess import call, check_output
 import sys
+import re
 
 def segment(inputF):
     with open(inputF, 'r') as f:
@@ -16,16 +19,17 @@ def segment(inputF):
             ####################### using segmenter ###################################
             output = check_output(["./segment" + " burmese2.fst '" + line + "'"], shell=True)
             outStr = output.decode('utf-8')
-            print(outStr)
             outStr = outStr.replace(" ", "|")
             outStr = outStr.replace("၊", "|၊|")
             outStr = outStr.replace("။", "|။")
             outStr = re.sub(r"(?P<punc>[\(\)\-\"\'])","|\g<punc>|", outStr)
             outStr = re.sub(r"(?P<eng>[0-9a-zA-Z]+)","|\g<eng>|", outStr)
-            outStr = re.sub(r"(?P<bur_digits>[၀-၉\.]+)","|\g<bur_digits>|", outStr)
+            outStr = re.sub(r"(?P<bur_digits>[၀-၉,\.]+)","|\g<bur_digits>|", outStr)
             outStr = outStr.replace("||", "|")
+            outStr = outStr.replace("|", "  ")
             outStr = outStr.replace("\n", '')
             ####################### end using segmenter
+            print(outStr)
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
